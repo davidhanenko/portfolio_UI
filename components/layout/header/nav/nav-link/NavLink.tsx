0008728422b035ea000d65fb7e186cb6a4691558
davidhanenko <d.hanenko24@gmail.com>
2022-04-14 22:28:19 -0400

    nav links / forwardRef

--- components/layout/header/nav/nav-link/NavLink.tsx
+++ components/layout/header/nav/nav-link/NavLink.tsx
@@ -1,22 +1,29 @@
 import Link from 'next/link';
-import React from 'react';
+import React, {
+  ForwardRefExoticComponent,
+  RefAttributes,
+} from 'react';
 import { NavLinkStyles } from './NavLinkStyles';
 
 type Props = {
   link: string;
-  href?: string;
-};
+  href?: string | undefined;
+  // onClick: (id: string) => void;
+} & React.ComponentPropsWithoutRef<'button'>;
 
-const LinkBnt = React.forwardRef<
-  HTMLButtonElement,
-  React.PropsWithChildren<Props>
->(({ href, link }, ref) => {
-  return (
-    <a href={href} href={ref}>
-      {link}
-    </a>
-  );
-});
+// eslint-disable-next-line react/display-name
+const LinkBnt = React.forwardRef(
+  (
+    { href, link }: Props,
+    ref: React.LegacyRef<HTMLAnchorElement> | undefined
+  ) => {
+    return (
+      <a href={href} ref={ref}>
+        {link}
+      </a>
+    );
+  }
+);
 
 export const NavLink: React.FC<Props> = ({ link }) => {
   return (

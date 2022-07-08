1925b73199aac1a990da1436300c309b25670047
davidhanenko <d.hanenko24@gmail.com>
2022-07-07 23:36:42 -0400

    project styles

--- components/projects/project/ProjectStyles.tsx
+++ components/projects/project/ProjectStyles.tsx
@@ -15,85 +15,18 @@ const fadeInR = keyframes`
   }
 `;
 
-const ProjectStyles = styled.div`
-  position: relative;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  padding-bottom: 20rem;
-
-  &:nth-of-type(2n + 1) {
-    .project-img {
-      margin-left: -20rem;
-      max-width: 70vw;
-      position: relative;
-      object-fit: contain;
-      height: 400px;
-      width: 700px;
-      cursor: pointer;
-    }
-
-    .project-title {
-      position: absolute;
-      top: -2rem;
-      right: -30%;
-
-      color: var(--orange);
-      font-size: 2.6rem;
-    }
+const fadeInL = keyframes`
+  0% {
+    opacity: 0;
+    transform: translate(-300%);
   }
-
-  &:nth-of-type(2n) {
-    .project-img {
-      margin-left: 20rem;
-      max-width: 70vw;
-      position: relative;
-      object-fit: contain;
-      height: 400px;
-      width: 700px;
-      cursor: pointer;
-    }
-
-    .project-title {
-      position: absolute;
-      top: -2rem;
-      left: -30%;
-
-      color: var(--orange);
-      font-size: 2.6rem;
-    }
-  }
-`;
-
-const LinksStyles = styled.section`
-  position: absolute;
-  bottom: 0;
-
-  &:nth-of-type(2n + 1) {
-    right: -25%;
-  }
-
-  &:nth-of-type(2n) {
-    left: -25%;
-  }
-
-  a {
-    font-size: 2rem;
-    color: var(--white);
-    padding: 1rem;
-    transition: all 0.25s;
-
-    &:hover svg {
-      color: var(--orange);
-      transform: scale(1.3);
-    }
+   100% {
+     opacity: 1;
+     transform: translate(0%);
   }
 `;
 
 const DescriptionStyles = styled.section<IProjectProps>`
-  position: absolute;
-  top: 25%;
-  right: -30%;
   width: 60%;
   height: 60%;
 
@@ -102,11 +35,7 @@ const DescriptionStyles = styled.section<IProjectProps>`
   cursor: auto;
   user-select: none;
   padding: 2rem;
-  transform: translate(300%);
   opacity: 0;
-
-  animation: ${props => props.inView && fadeInR} 1s;
-  animation-fill-mode: forwards;
 
   .project-type {
     display: flex;
@@ -134,6 +63,102 @@ const DescriptionStyles = styled.section<IProjectProps>`
 
     &::marker {
       color: var(--lightOrange);
+    }
+  }
+`;
+
+const LinksStyles = styled.div`
+  a {
+    font-size: 2rem;
+    color: var(--white);
+    padding: 1rem;
+    transition: all 0.25s;
+
+    &:hover svg {
+      color: var(--orange);
+      transform: scale(1.3);
+    }
+  }
+`;
+
+const ProjectStyles = styled.div`
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding-bottom: 20rem;
+
+  &:nth-of-type(2n + 1) {
+    .project-img {
+      margin-left: -20rem;
+      max-width: 70vw;
+      position: relative;
+      object-fit: contain;
+      height: 400px;
+      width: 700px;
+      cursor: pointer;
+    }
+
+    .project-title {
+      position: absolute;
+      top: -2rem;
+      right: -30%;
+
+      color: var(--orange);
+      font-size: 2.6rem;
+    }
+
+    ${LinksStyles} {
+      position: absolute;
+      bottom: 0;
+      right: -25%;
+    }
+
+    ${DescriptionStyles} {
+      position: absolute;
+      top: 25%;
+      right: -30%;
+      transform: translate(300%);
+      animation: ${(props: IProjectProps) =>
+          props.inView && fadeInR}
+        1s;
+      animation-fill-mode: forwards;
+    }
+  }
+
+  &:nth-of-type(2n) {
+    .project-img {
+      margin-left: 20rem;
+      max-width: 70vw;
+      position: relative;
+      object-fit: contain;
+      height: 400px;
+      width: 700px;
+      cursor: pointer;
+    }
+
+    .project-title {
+      position: absolute;
+      top: -2rem;
+      left: -30%;
+
+      color: var(--orange);
+      font-size: 2.6rem;
+    }
+
+    ${LinksStyles} {
+      position: absolute;
+      bottom: 0;
+      left: -25%;
+    }
+
+    ${DescriptionStyles} {
+      position: absolute;
+      top: 25%;
+      right: -30%;
+      transform: translate(-300%);
+      animation: ${props => props.inView && fadeInL} 1s;
+      animation-fill-mode: forwards;
     }
   }
 `;
